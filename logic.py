b1992756fe1f9acf1a96120537bad4f44e4a7141
goswamianjali <77376266+goswamianjali@users.noreply.github.com>
2022-11-02 15:44:31 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,12 +1,12 @@
-sentence = input('\n Enter the words:')
-print(sentence)
-discount = int(input('\n Discount value:'))
-s = sentence.split()
-#len(s)
-for x in range(len(s)):
+def discount():
+  sentence = input('\n Enter the words:')
+  print(sentence)
+  discount = int(float(input('\n Discount value:')))
+  s = sentence.split()
+  for x in range(len(s)):
 
-            if s[x][0] =='$':
-              price = int(float(s[x][1:]))
+            if s[x][0] =='$' and s[x][1:].isnumeric():
+              price = float(s[x][1:])
               new_price = ''
               new_price+=s[x][0]
               dis = (price*discount) / 100
@@ -16,8 +16,9 @@ for x in range(len(s)):
               #print("converted values into decimal after applying discount are:")
               print(s[x], end = ' ')
 
-            elif s[x][0] !='$':
+            elif s[x][0] !='$' or not s[x][1:].isnumeric():
                 #print("Not converted values into decimal after applying discount are:")
               print(s[x], end = ' ')
             else:
               continue
+print(discount())
